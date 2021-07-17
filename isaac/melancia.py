67f5c168e0d9f586261a5e31875a6c4ba93573c1
isaac-prado <isaacpalmeida@unifei.edu.br>
2021-07-17 13:58:15 -0300

    Jogo de Isaac

--- isaac/melancia.py
+++ isaac/melancia.py
@@ -1,4 +1,9 @@
 # simulação de plantação de melancia
+'''
+i (erva daninha) = capinar -> 20% de semente
+
+
+'''
 
 import curses
 
@@ -13,3 +18,18 @@ curses.beep()
 window = curses.newwin(LIN, COL, 0, 0)
 window.keypad(1)
 window.border('X', 'X', 'X', 'X')
+
+def irrigar(agua):
+    if(agua > 0):
+        lin = input('IRRIGAR[linha]: ')
+        col = input('IRRIGAR[coluna]: ')
+        
+        if((matriz[lin][col] == ' ') and (lin < 25) and (col < 50)):        # :(
+            matriz[lin][col] = '~'
+            agua--
+        else:
+            print ('\nATENCAO: Posicao invalida ou ja ocupada!\n')
+    else:
+        print('\nATENCAO: Sem saldo de agua!\n')
+    
+
