8c66609d7aed87cf96dec126cb7fabf637c37de2
isaac-prado <isaacpalmeida@unifei.edu.br>
2021-07-17 14:47:03 -0300

    Isaac

--- isaac/melancia.py
+++ isaac/melancia.py
@@ -19,17 +19,3 @@ window = curses.newwin(LIN, COL, 0, 0)
 window.keypad(1)
 window.border('X', 'X', 'X', 'X')
 
-def irrigar(agua):
-    if(agua > 0):
-        lin = input('IRRIGAR[linha]: ')
-        col = input('IRRIGAR[coluna]: ')
-        
-        if((matriz[lin][col] == ' ') and (lin < 25) and (col < 50)):        # :(
-            matriz[lin][col] = '~'
-            agua--
-        else:
-            print ('\nATENCAO: Posicao invalida ou ja ocupada!\n')
-    else:
-        print('\nATENCAO: Sem saldo de agua!\n')
-    
-
